b61f92a7a4e3292abcb20d0317a82e35b6d00bcf
Ehab Ahmed <ehab.ahmed0202@gmail.com>
2025-03-21 20:39:27 +0200

    one-to-one bi relationship

--- src/main/java/com/luv2code/cruddemo/dao/InstructorDAOImpl.java
+++ src/main/java/com/luv2code/cruddemo/dao/InstructorDAOImpl.java
@@ -1,6 +1,7 @@
 package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,5 +41,25 @@ public class InstructorDAOImpl implements InstructorDAO {
         //delete instructor
         entityManager.remove(tempInstructor);
         //will delete detail too because CascadeType.ALL
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int theId) {
+        return entityManager.find(InstructorDetail.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int theId) {
+
+        InstructorDetail tempInstructorDetail = entityManager.find(InstructorDetail.class, theId);
+
+        //remove the associated object reference
+        //break the bi-direction link
+        //
+        tempInstructorDetail.getInstructor().setInstructorDetail(null);
+
+        entityManager.remove(tempInstructorDetail);
+
     }
 }
